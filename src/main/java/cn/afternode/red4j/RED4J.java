f964f6b1a219eaa9c0e7bacb7a7212c1c21f602a
Zyklone <h3xadecimal@afternode.cn>
2024-11-04 19:37:04 +0800

    java part basic

---
+++ src/main/java/cn/afternode/red4j/RED4J.java
@@ -0,0 +1,29 @@
+package cn.afternode.red4j;
+
+import cn.afternode.red4j.util.ClasspathAppender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class RED4J {
+    private static Logger logger;
+
+    public static void initialize() throws IOException {
+        // Inject dependencies
+        try {
+            ClassLoader loader = RED4J.class.getClassLoader();
+            for (File file : new File(RED4J.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().listFiles()) {
+                if (file.getName().endsWith(".jar"))
+                    ClasspathAppender.app(file.toURI().toURL(), loader);
+            }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger = LoggerFactory.getLogger("RED4J");
+        logger.info("Classpath injection completed");
+    }
+}
---
+++ src/main/java/cn/afternode/red4j/util/ClasspathAppender.java
@@ -0,0 +1,84 @@
+package cn.afternode.red4j.util;
+
+import sun.misc.Unsafe;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class ClasspathAppender {
+    private static Unsafe unsafe;
+    private static MethodHandles.Lookup lookup;
+
+    private static void init() {
+        try {
+            Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+            unsafeField.trySetAccessible();
+            unsafe = (Unsafe) unsafeField.get(null);
+
+            Field lookupField = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+            lookup = (MethodHandles.Lookup) unsafe.getObject(unsafe.staticFieldBase(lookupField), unsafe.staticFieldOffset(lookupField));
+        } catch (Throwable t) {
+            throw new ExceptionInInitializerError(t);
+        }
+    }
+
+    public static void app(URL url, ClassLoader ldr) {
+        Field ucpField;
+        try {
+            ucpField = ldr.getClass().getDeclaredField("ucp");
+        } catch (NoSuchFieldException e) {
+            try {
+                ucpField = ldr.getClass().getSuperclass().getDeclaredField("ucp");
+            } catch (NoSuchFieldException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+        try {
+            addURL(ldr, ucpField, url);
+        } catch (Throwable t) {
+            throw new RuntimeException("addURL failed", t);
+        }
+    }
+
+    /**
+     * Append to current AppClassLoader
+     */
+    public static void app(URL url) {
+        app(url, ClasspathAppender.class.getClassLoader());
+    }
+
+    /**
+     * Append to URLClassLoader
+     */
+    public static void ucl(URL url, URLClassLoader ldr) {
+        try {
+            MethodHandle mh = lookup.findVirtual(URLClassLoader.class, "addURL", MethodType.methodType(void.class, URL.class));
+            mh.invoke(ldr, url);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Append to current URLClassLoader
+     */
+    public static void ucl(URL url) {
+        ucl(url, (URLClassLoader) ClasspathAppender.class.getClassLoader());
+    }
+
+    private static void addURL(ClassLoader ldr, Field ucpField, URL url) throws Throwable {
+        Object ucp = unsafe.getObject(ldr, unsafe.objectFieldOffset(ucpField));
+
+        MethodHandle hnd = lookup.findVirtual(ucp.getClass(), "addURL", MethodType.methodType(void.class, URL.class));
+        hnd.invoke(ucp, url);
+    }
+
+    static {
+        init();
+    }
+}
